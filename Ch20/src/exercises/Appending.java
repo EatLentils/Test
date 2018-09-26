a7bd6e6dcc7f6635e1c4061d668810f9843d97a6
nmarshall <nmarshall@ATC-137-PD11D.sau40.com>
2018-09-26 13:54:25 -0400

    Me Comment

--- Ch20/src/exercises/Appending.java
+++ Ch20/src/exercises/Appending.java
@@ -12,7 +12,6 @@ public class Appending {
 			list1.add(list2.get(i));
 		}
 	}
-	//yes
 	public static <E> void append4a(List<E> list1, List<E> list2){
 		ListIterator it = list2.listIterator();
 		while(it.hasNext()){
